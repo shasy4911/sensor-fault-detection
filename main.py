6236d371150f7abc56a5bae97485d54d32441b3f
Shashank <shashanksaurav4911@gmail.com>
2022-11-03 18:44:44 +0530

    Adding Data Ingestion Component code and artifacts

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import logging
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.constant import training_pipeline
+from sensor.exception import SensorException
+import os, sys
+from sensor.logger import logging
+#from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+from sensor.pipeline.training_pipeline import TrainPipeline
+
+"""def test_exception():
+    try:
+        logging.info("We are dividing 1 by 0")
+        x = 1/0
+    except Exception as e:
+        raise SensorException(e, sys)"""
 
 if __name__ == '__main__':
-    mongodb_client = MongoDBClient()
-    print("collection_name: ",mongodb_client.database.list_collection_names())
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
+
+    
+    
+    """training_pipeline_config = TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict__)"""
+    
+    """mongodb_client = MongoDBClient()
+    print("collection_name: ",mongodb_client.database.list_collection_names())"""
+    
+    """try:
+        test_exception()
+    except Exception as e:
+        print(e)"""
+    
--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,20 +1,24 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
 import certifi
+import os
+from sensor.constant.env_variable import MONGO_URL_KEY
+
 ca = certifi.where()
 
 class MongoDBClient:
     client = None
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
+            mongo_db_url = os.getenv("MONGO_URL_KEY")
             if MongoDBClient.client is None:
-                mongo_db_url  = "mongodb+srv://shashank:4911ssssdsad@cluster0.6kpihcd.mongodb.net/test"
+                print("Inside if block MongoDBClient file")
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
+            print("MongoDBClient Database: ", self.database)
             self.database_name = database_name
+            print("MongoDBClient DatabaseName: ", self.database_name)
         except Exception as e:
             raise e
 
-#mongodb+srv://avnish:Aa327030@cluster0.or68e.mongodb.net/admin?authSource=admin&replicaSet=atlas-desfdx-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true
-#mongodb+srv://shashank:4911ssssdsad@cluster0.6kpihcd.mongodb.net/test
